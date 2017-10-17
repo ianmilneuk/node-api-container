4826ed7b93080c6fef027cd0cf27c477f0bef56c
W Brian Leonard <william.leonard@oracle.com>
2017-10-17 11:39:29 -0400

    Added code test routine

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
     //res.send('Hello World!');
-    var html = fs.readFileSync(APP_HOME+'/home.html').toString();
+    var html = fs.readFileSync(APP_HOME + '/home.html').toString();
     res.send(html);
 });
 
@@ -37,7 +37,7 @@ app.get('/', function (req, res) {
 app.get('/platforms/:platformId/domains/:domainId/accounts/:accountId', function (req, res) {
 
     console.log(req.body);
-    
+
     var platformId = req.params.platformId;
     var domainId = req.params.domainId;
     var accountId = req.params.accountId;
@@ -61,7 +61,7 @@ app.get('/platforms/:platformId/domains/:domainId/accounts/:accountId', function
 app.post('/platforms/:platformId/domains/:domainId/accounts/:accountId', function (req, res) {
 
     console.log(req.body);
-    
+
     var platformId = req.params.platformId;
     var domainId = req.params.domainId;
     var accountId = req.params.accountId;
@@ -83,5 +83,75 @@ app.post('/platforms/:platformId/domains/:domainId/accounts/:accountId', functio
 })
 
 app.listen(PORT, function () {
-    console.log('Example app listening on port ' + PORT + '!');
+
+    //codeTest();
+    console.log('Node API Container listening on port ' + PORT + '!');
 })
+
+function codeTest() {
+
+    var url = require('url');
+    var REQUEST_URL = "http://www.oracle.com/test?subjectx=astronomy"
+
+    var url_parts = url.parse(REQUEST_URL, true);
+    var query = url_parts.query;
+    var subject = ((typeof query.subject !== 'undefined') ? query.subject : "");
+    var relax = ((typeof query.relax !== 'undefined') ? query.relax : "false");
+
+    console.log("REQUEST_URL = " + REQUEST_URL);
+    console.log(url_parts);
+    console.log("subject: " + subject);
+    console.log("relax: " + relax);
+
+    if (subject.toLowerCase() === 'astronomy' || relax === "true") {
+
+        var response =
+                {
+                    "recordsFound": 3,
+                    "searchTerm": subject,
+                    "relaxed": relax,
+                    "results": [
+                        {
+                            "title": "The Cosmic Perspective, 2nd edition",
+                            "authors": [
+                                "Bennett",
+                                "Donahue",
+                                "Schneider",
+                                "Voit"
+                            ],
+                            "publisher": "Innovation Education",
+                            "cover_image": "http://www.webassign.net/bdsvastro/BDSVastr02_cover_sm.jpg"
+                        },
+                        {
+                            "title": "Astronomy Today, 8th edition",
+                            "authors": [
+                                "Chaisson ",
+                                "McMillan"
+                            ],
+                            "publisher": "Pearson Education",
+                            "cover_image": "http://www.webassign.net/chaastrot8/chaastrot8_cover_sm.jpg"
+                        },
+                        {
+                            "title": "Astronomy: A Beginner's Guide to the Universe, 5th edition",
+                            "authors": [
+                                "Chaisson ",
+                                "McMillan"
+                            ],
+                            "publisher": "Pearson Education",
+                            "cover_image": "http://www.webassign.net/bg/bg5_cover_sm.jpg"
+                        }
+                    ]
+                };
+
+    } else {
+        var response =
+                {
+                    "recordsFound": 0,
+                    "searchTerm": subject,
+                    "relaxed": relax
+                };
+    }
+
+    console.log(response);
+
+}
